92dc19937049da42eae2b66e10bb7909c2c295fe
xxrachel <dev.rachelxx@gmail.com>
2022-07-31 17:02:41 +0900

    2022.07.31 base start

---
+++ w-board-service/src/main/java/com/wom/board/WBoardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wom.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WBoardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WBoardServiceApplication.class, args);
+	}
+
+}
---
+++ w-board-service/src/test/java/com/wom/board/WBoardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wom.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WBoardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
